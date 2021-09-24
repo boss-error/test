e0b4c677cd51fd8da3602ad2737d63aa825e8c55
Boss <57038976+boss-error@users.noreply.github.com>
2021-09-24 02:42:31 +0200

    Add files via upload

--- try.py
+++ try.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 
 #set socks5 proxy to use tor
 
-socks.set_default_proxy(socks.SOCKS5, "209.127.191.180", 9279)
+socks.set_default_proxy(socks.SOCKS5, "185.132.177.153", 1080)
 socket.socket = socks.socksocket
 req = Request('https://sultandx.com', headers={'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 10; SM-A105F Build/QP1A.190711.020)', })
 html = urlopen(req).read()
